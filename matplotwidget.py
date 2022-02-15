14a507c5e344e57aa457f468935181749d64dfc8
Slavlen <slavlen2001@gmail.com>
2022-02-15 04:43:21 +0300

    Update matplotwidget.py

--- matplotwidget.py
+++ matplotwidget.py
@@ -28,7 +28,7 @@ class MatPlotWidget(QWidget):
     def update_screen(self, figure, params):
         self.canvas.axes.clear()
 
-        if CreateFigure.get_figure() == "круг":
+        if figure == "круг":
             X = params[0]
             u = np.linspace(0, 2 * np.pi, 100)
             x = np.cos(u) * X
@@ -36,18 +36,18 @@ class MatPlotWidget(QWidget):
             self.canvas.axes.plot(x, y, 0)
             self.canvas.axes.set_box_aspect(aspect=(X, X, 0))
 
-        elif CreateFigure.get_figure() == "квадрат":
+        elif figure == "квадрат":
             x = params[0]
             self.canvas.axes.plot([0, x, x, 0, 0], [0, 0, x, x, 0], [0, 0, 0, 0, 0])
             self.canvas.axes.set_box_aspect(aspect=(x, x, 0))
 
-        elif CreateFigure.get_figure() == "прямоугольник":
+        elif figure == "прямоугольник":
             x = params[0]
             y = params[1]
             self.canvas.axes.plot([0, x, x, 0, 0], [0, 0, y, y, 0], [0, 0, 0, 0, 0])
             self.canvas.axes.set_box_aspect(aspect=(x, y, 0))
 
-        elif CreateFigure.get_figure() == "треугольник":
+        elif figure == "треугольник":
             x = params[0]
             y = params[1]
             a = params[2] * np.pi / 180
@@ -56,14 +56,14 @@ class MatPlotWidget(QWidget):
             self.canvas.axes.plot([0, x, (x - endx), 0], [0, 0, y, 0], [0, 0, 0, 0])
             self.canvas.axes.set_box_aspect(aspect=(x, y, 0))
 
-        elif CreateFigure.get_figure() == "трапеция":
+        elif figure == "трапеция":
             x = params[0]
             y = params[1]
             h = params[2]
             self.canvas.axes.plot([0, 0, y, x, 0], [0, h, h, 0, 0], [0, 0, 0, 0, 0])
             self.canvas.axes.set_box_aspect(aspect=(x, h, 0))
 
-        elif CreateFigure.get_figure() == "ромб":
+        elif figure == "ромб":
             x = params[0]
             a = params[1] * np.pi / 180
             b = (180 - params[1]) * np.pi / 180
@@ -85,7 +85,7 @@ class MatPlotWidget(QWidget):
             self.canvas.axes.set_ylim3d(0, x - endx)
             self.canvas.axes.set_zlim3d(0, 0)
 
-        elif CreateFigure.get_figure() == "сфера":
+        elif figure == "сфера":
             u = np.linspace(0, 2 * np.pi, 100)
             v = np.linspace(0, np.pi, 100)
             x = params[0] * np.outer(np.cos(u), np.sin(v))
@@ -94,7 +94,7 @@ class MatPlotWidget(QWidget):
             self.canvas.axes.plot_surface(x, y, z)
             self.canvas.axes.set_box_aspect(aspect=(params[0], params[0], params[0]))
 
-        elif CreateFigure.get_figure() == "куб":
+        elif figure == "куб":
             x = params[0]
             self.canvas.axes.plot([0, x, x, 0, 0], [0, 0, x, x, 0], [0, 0, 0, 0, 0], color='b')
             self.canvas.axes.plot([0, 0, x, x], [0, 0, 0, 0], [0, x, x, 0], color='b')
@@ -103,7 +103,7 @@ class MatPlotWidget(QWidget):
             self.canvas.axes.plot([0, 0, 0], [x, x, 0], [0, x, x], color='b')
             self.canvas.axes.set_box_aspect(aspect=(x, x, x))
 
-        elif CreateFigure.get_figure() == "паралепипед":
+        elif figure == "паралепипед":
             x = params[0]
             y = params[1]
             z = params[2]
@@ -114,7 +114,7 @@ class MatPlotWidget(QWidget):
             self.canvas.axes.plot([0, 0, 0], [y, y, 0], [0, z, z], color='b')
             self.canvas.axes.set_box_aspect(aspect=(x, y, z))
 
-        elif CreateFigure.get_figure() == "пирамида":
+        elif figure == "пирамида":
             def pc(t):
                 x = np.cos(t) * (np.cos(an / 2) / (np.cos(an * (t / an - np.floor(t / an)) - an / 2)))
                 return x
@@ -138,7 +138,7 @@ class MatPlotWidget(QWidget):
             self.canvas.axes.plot_surface(x, y, z)
             self.canvas.axes.set_box_aspect(aspect=(params[0], params[0], params[2]))
 
-        elif CreateFigure.get_figure() == "цилиндр":
+        elif figure == "цилиндр":
             def data_for_cylinder_along_z(center_x, center_y, radius, height_z):
                 z = np.linspace(0, height_z, 50)
                 theta = np.linspace(0, 2 * np.pi, 50)
@@ -151,7 +151,7 @@ class MatPlotWidget(QWidget):
             self.canvas.axes.plot_surface(x, y, z)
             self.canvas.axes.set_box_aspect(aspect=(x, y, z))
 
-        elif CreateFigure.get_figure() == "конус":
+        elif figure == "конус":
             theta = np.linspace(0, 2 * np.pi, 100)
             r = np.linspace(0, 2, 100)
             t, R = np.meshgrid(theta, r)
